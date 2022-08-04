d8756a9b865d477d8186ce96191d0d88b25617f2
Ludovic <ludovic@localhost.localdomain>
2022-08-04 16:01:46 +0200

    correction accents pour intégration patchs

--- defaults/pref/commentaires.js
+++ defaults/pref/commentaires.js
@@ -3,6 +3,6 @@ pref("mailnews.tags.~commente.color", "#666666");
 pref("mailnews.tags.~commente.tag", "Comment\u00e9");
 
 
-// si true, efface les étiquettes apres ajout d'un commentaire
+// si true, efface les etiquettes apres ajout d'un commentaire
 pref("commentaires.razetiqettes", false);
 
